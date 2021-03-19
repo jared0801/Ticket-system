67184cd829f23d1ff332372364ad336b88216445
jacobs67 <jared0801@gmail.com>
2021-03-19 13:18:19 -0700

    Added mobile menu for global header

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -4,18 +4,35 @@
         color="primary"
         dark
         >
-      <div>
-        <v-btn text active-class="active-home" class="button ml-2 mr-2 primary" :to="isLoggedIn ? '/dashboard' : '/'">
-            <h1 class="title white--text">Ticket System</h1>
-        </v-btn>
-      </div>
+        <div>
+            <v-btn text active-class="active-home" class="button ml-2 mr-2 primary" :to="isLoggedIn ? '/dashboard' : '/'">
+                <h1 class="title white--text">Ticket System</h1>
+            </v-btn>
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-        <div class="d-flex align-center" v-if="isLoggedIn">
-            <p class="mb-0 mr-5">Welcome back, {{ username }}</p>
+        <div class="d-none d-sm-flex align-center" v-if="isLoggedIn">
+            <p class="mb-0 mr-5">Welcome, {{ username }}</p>
             <v-btn to="/profile" text>Profile</v-btn>
             <v-btn @click="logout" text>Logout</v-btn>
+        </div>
+        <div class="d-sm-none" v-if="isLoggedIn">
+            <v-menu offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="secondary" dark v-bind="attrs" v-on="on">
+                        Menu
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item
+                    v-for="(item, index) in items"
+                    :key="index" @click="item.action"
+                    >
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </div>
     </v-app-bar>
 </template>
@@ -26,7 +43,15 @@ import { mapState, mapMutations, mapGetters } from 'vuex';
 
 export default {
     name: "GlobalHeader",
-    props: {
+    data() {
+        return {
+            items: [
+                { title: "Home", action: () => this.$router.push('/') },
+                { title: "Profile", action: () => this.$router.push('/profile') },
+                { title: "Logout", action: () => this.logout() }
+            ],
+            selected: '',
+        }
     },
     computed: {
         ...mapState('user', ['isLoggedIn', 'username']),
@@ -50,6 +75,12 @@ export default {
                 this.storeUser(user);
             }
         });
+    },
+    watch: {
+        $route: function() {
+            console.log('change');
+            this.selected = null;
+        }
     }
 }
 </script>
@@ -63,4 +94,8 @@ export default {
 .active-home:hover:before {
     opacity: 0.18 !important;
 }
+.select {
+    margin-top: 1.5em !important;
+}
+
 </style>
